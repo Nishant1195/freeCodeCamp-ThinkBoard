b68a9c867f1315f5371c87b896c46cc23ffe8217
Nishant1195 <nishant8830875196@gmail.com>
2025-07-29 22:29:32 +0530

    Controllers Changed

--- backend/src/controller/noteController.js
+++ backend/src/controller/noteController.js
@@ -1,9 +1,54 @@
-import express from "express";
+import express, { json } from "express";
+import Notes from "../model/notes.js";
 
-export const getALLNotes = (req, res) => {res.send("fetched data successfully")};
+export const getALLNotes = async (req, res) => {
+    try {
+        const notes = await Notes.find();
+        res.status(200).json(notes);
+    } catch (error) {
+        console.log("Error at getAllNotes");
+        res.status(500).json("Internal Server Error")
+    }
+};
 
-export const createNote = (req, res) => {res.send("data posted successfully")};
+export const createNote = async (req, res) => {
+    try {
+        const {title, content} = req.body;
+        const Note = new Notes({title, content});
+    
+        const savedNote = await Note.save();
+        res.status(201).json(savedNote);
+        
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({message: "Internal Server Error!"});
+        
+    }
+};
 
-export const updateNote = (req,res) => {res.send("Data updated successfully")};
+export const updateNote = async (req,res) => {
+    try {
+        const {title, content} = req.body;
+        await Notes.findByIdAndUpdate(req.params.id, {title, content});
 
-export const deleteNote = (req, res) => {res.send("Data deleted successfully")};
+        res.status(201).json({message:"Note updated successfully"});
+    } catch (error) {
+        console.log(error);
+        res.send(500).json({error:"Internal server error"})
+        
+    }
+};
+
+export const deleteNote = async (req, res) => {
+    try {
+        const deleteNote = req.params.id;
+        await Notes.findByIdAndDelete(deleteNote);
+
+        res.status(201).json({message:"Note Deleted Successfully"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:"Internal Server Error"});
+        
+    }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -8,8 +8,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use("/api/notes", noteRoutes);
 connectDB();
+
+
 
 app.listen(PORT, () => {
     console.log("Server is running on port: ", PORT);
